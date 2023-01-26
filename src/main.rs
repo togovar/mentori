8cd4de106942cbcdebf1c9eb24bb34d9a534d899
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2023-01-27 00:52:22 +0900

    print duplicated record

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use mentori::normalize;
 use rust_htslib::bcf;
 use rust_htslib::bcf::Read;
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::path::PathBuf;
 use std::str;
 use structopt::clap::crate_description;
@@ -26,6 +28,8 @@ struct Options {
     pub input: Option<PathBuf>,
 }
 
+type Cache = HashMap<(u32, i64, String, String), Vec<(u32, i64, String, String, String)>>;
+
 pub fn main() {
     let options = Options::from_args();
 
@@ -44,42 +48,49 @@ pub fn main() {
     };
 
     if options.check {
-        let mut buf = Vec::new();
         let mut pos = 0;
+        let mut map: Cache = HashMap::new();
 
         for r in vcf.records() {
             let record = r.unwrap();
 
-            if pos != record.pos() {
-                buf.clear();
+            if record.allele_count() > 2 {
+                continue;
             }
 
             let alleles = record.alleles();
+
             if let Some((reference, alternate)) = alleles.split_first() {
-                let keys = (
+                if pos != record.pos() {
+                    print_duplicates(record.header(), &mut map);
+                    map.clear();
+                }
+
+                let key = (
                     record.rid().unwrap(),
+                    record.pos(),
                     String::from_utf8(reference.to_vec()).unwrap(),
                     String::from_utf8(alternate[0].to_vec()).unwrap(),
                 );
 
-                if buf.contains(&keys) {
-                    let name = record.header().rid2name(record.rid().unwrap()).unwrap();
-
-                    println!(
-                        "VCF has duplicated record: {}\t{}\t{}\t{}\t{}",
-                        str::from_utf8(name).unwrap(),
-                        record.pos() + 1,
-                        str::from_utf8(record.id().as_slice()).unwrap(),
-                        str::from_utf8(reference).unwrap(),
-                        str::from_utf8(alternate[0]).unwrap()
-                    )
+                if !map.contains_key(&key) {
+                    map.insert(key.to_owned(), Vec::new());
                 }
 
-                buf.push(keys);
-            }
+                let vec = map.get_mut(&key).unwrap();
+                vec.push((
+                    record.rid().unwrap(),
+                    record.pos(),
+                    String::from_utf8(record.id()).unwrap(),
+                    String::from_utf8(reference.to_vec()).unwrap(),
+                    String::from_utf8(alternate[0].to_vec()).unwrap(),
+                ));
 
-            pos = record.pos();
+                pos = record.pos();
+            }
         }
+
+        print_duplicates(vcf.header(), &mut map);
     } else {
         let header = bcf::Header::from_template(vcf.header());
         let mut output = bcf::Writer::from_stdout(&header, true, bcf::Format::Vcf).unwrap();
@@ -90,6 +101,26 @@ pub fn main() {
             if normalize(&mut record).is_ok() {
                 output.write(&record).unwrap_or(());
             }
+        }
+    }
+}
+
+fn print_duplicates(header: &bcf::header::HeaderView, map: &mut Cache) {
+    let dups = map
+        .iter()
+        .filter(|(_k, v)| v.len() > 1)
+        .collect::<HashMap<_, _>>();
+
+    for dup in dups.values() {
+        for r in dup.iter() {
+            println!(
+                "{}\t{}\t{}\t{}\t{}",
+                str::from_utf8(header.rid2name(r.0).unwrap()).unwrap(),
+                r.1 + 1,
+                r.2,
+                r.3,
+                r.4
+            )
         }
     }
 }
